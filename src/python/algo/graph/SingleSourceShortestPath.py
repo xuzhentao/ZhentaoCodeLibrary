29f89091d18f22b4060618a2d5ff4f8f6aef44ac
Zhentao Xu <frankxu@umich.edu>
2022-03-25 18:01:44 -0700

    [S] dev Bellman Ford Algorithm for Single Source Shortest Path

--- src/python/algo/graph/SingleSourceShortestPath.py
+++ src/python/algo/graph/SingleSourceShortestPath.py
@@ -1,16 +1,24 @@
 ######################################################################################
 #	Author: 				Zhentao Xu (frankxu@umich.edu)
 #	Date: 					2022/3/24
-#	Description: 			Single Source Shortest Path in Graph
+#	Description: 			Single Source The Shortest Path in Graph
 #	Tested Environment:		Macbook Pro w/ Python 3.9
 #	Required Libraries:		N/A
 ######################################################################################
-
-
+import copy
 import heapq
 import pprint
 import sys
 from typing import *
+
+
+def helper_visualize_shortest_path_result(d: Dict[str, Tuple[int, str]]) -> None:
+    for node in d:
+        path = [node]
+        while d[path[-1]][1] != '-':
+            path.append(d[path[-1]][1])
+        print(
+            "node = " + node + "\t shortest_distance = " + str(d[node][0]) + "\t path from source = " + str(path[::-1]))
 
 
 class SingleSourceShortestPath:
@@ -22,12 +30,19 @@ class SingleSourceShortestPath:
                                                         "D": [("C", 1), ("A", 3), ("B", 2), ("E", 2)],
                                                         "E": [("B", 1), ("D", 2)]}
 
-    def dijkstra_algorithm(self, starting_point: str = "A") -> Dict[str, int]:
+        self.graph_neg: Dict[str, List[Tuple[str, int]]] = {"A": [("B", 100), ("C", 500), ("D", 200)],
+                                                            "B": [("C", 100)], "C": [("D", 100)], "D": [("B", -150)]}
+
+    def dijkstra_algorithm(self, graph, starting_point: str = "A") -> Dict[str, Tuple[int, str]]:
         """
+        pre-requist: the graph has NO negative value weights.
+        find the shortest path from starting_point to all points.
+        Time complexity: O((v+e)logv) #
         :return: a distance dictionary for all vertex with minimal distance from starting_point to each vertex.
         """
-        dist: Dict[str, int] = {k: sys.maxsize for k in self.graph.keys()}
-        dist[starting_point] = 0
+        dist: Dict[str, Tuple[int, str]] = {k: (sys.maxsize, None) for k in
+                                            graph.keys()}  # the second element is the parent.
+        dist[starting_point] = (0, "-")
 
         S: Set[str] = set()
         Q: List[Tuple[int, str]] = [(0, starting_point)]
@@ -40,23 +55,75 @@ class SingleSourceShortestPath:
             curr_dist, curr_node = heapq.heappop(Q)
             print("\t popped node and its distance = " + str(curr_node) + " dist = " + str(curr_dist))
             S.add(curr_node)
-            for neighbor_node, neighbor_dist in self.graph[curr_node]:
+            for neighbor_node, neighbor_dist in graph[curr_node]:
                 if neighbor_node is S:
                     continue
                 print("\tcomparing the current distance " + str(
                     dist[neighbor_node]) + " and the potentially shorter distance from curr_node " + str(
                     curr_node) + " and neighbor " + str(neighbor_node) + " with distance " + str(
                     neighbor_dist + curr_dist))
-                if neighbor_dist + curr_dist < dist[neighbor_node]:
-                    dist[neighbor_node] = neighbor_dist + curr_dist
-                    heapq.heappush(Q, (dist[neighbor_node], neighbor_node))
+                if neighbor_dist + curr_dist < dist[neighbor_node][0]:
+                    dist[neighbor_node] = (neighbor_dist + curr_dist, curr_node)
+                    heapq.heappush(Q, (dist[neighbor_node][0], neighbor_node))
                     print("\t\tshorter distance founded, added the node " + str(neighbor_node) + " to the heapq")
                 else:
                     print("\t\tkeep the current dist")
 
+        helper_visualize_shortest_path_result(dist)
         return dist
+
+    def bellman_ford_algorithm(self, graph: Dict[str, List[Tuple[str, int]]]) -> None:
+        """
+        Algorithm introduction links: https://leetcode.com/explore/learn/card/graph/622/single-source-shortest-path-algorithm/3864/
+        :param graph:
+        :return:
+        """
+        import numpy as np
+        np.set_printoptions(linewidth=np.inf)
+
+        def print_dp(dp):
+            import copy
+            dp_cp = copy.deepcopy(dp)
+            for i in range(len(dp_cp)):
+                for j in range(len(dp_cp[0])):
+                    dp_cp[i][j] = str(dp_cp[i][j])
+            print(np.array(dp_cp))
+
+        # simple conversion on node name from str to int
+        map_node_to_idx = {n: i for i, n in enumerate(sorted(graph.keys()))}
+        graph: Dict[int, Dict[int, int]] = {map_node_to_idx[k]: {map_node_to_idx[k1]: v1 for k1, v1 in v} for k, v in
+                                            graph.items()}
+        n_node = len(graph)
+        edges: List[Tuple[int, int, int]] = [(s, t, d) for s in graph.keys() for t, d in graph[s].items()]
+
+        # step-2 initialize the DP table
+        # row - i is the maximum used edge, column - j is the target to reach node j
+        dp: List[List[(int, int)]] = [[(sys.maxsize, None)] * n_node for _ in range(n_node)]
+        for i in range(n_node):
+            dp[i][0] = (0, None)
+        print("initial DP = " )
+        print_dp(dp)
+
+        # step 3, DP
+        for i in range(1, n_node ):
+            print("=======Iteration = " + str(i) + "===========")
+            for s, t, d in edges:
+                print('processing edge from ' + str(s)  + " to " + str(t) + " with weight " + str(d))
+                if dp[i][t][0] > dp[i-1][s][0] + graph[s][t]:
+                    print("updating")
+                    dp[i][t] = (dp[i-1][s][0] + graph[s][t], s)
+                print_dp(dp)
+
+        dist = dict()
+        map_idx_to_node = {v:k for k,v in map_node_to_idx.items()}
+        for t, dist_p in enumerate(dp[-1]):
+            dist[map_idx_to_node[t]] = (dist_p[0],map_idx_to_node[dist_p[1]] if dist_p[1] is not None else '-')
+        helper_visualize_shortest_path_result(dist)
+        return dist
+
 
 
 if __name__ == "__main__":
     sssp = SingleSourceShortestPath()
-    pprint.pprint(sssp.dijkstra_algorithm())
+    pprint.pprint(sssp.dijkstra_algorithm(graph=sssp.graph))
+    pprint.pprint(sssp.bellman_ford_algorithm(graph=sssp.graph_neg))
--- test/python/algo/graph/test_single_source_shortest_path.py
+++ test/python/algo/graph/test_single_source_shortest_path.py
@@ -5,6 +5,20 @@ from src.python.algo.graph.SingleSourceShortestPath import SingleSourceShortestP
 
 def test_dijkstra_algorithm():
     sssp = SingleSourceShortestPath()
-    res = sssp.dijkstra_algorithm()
+    res = sssp.dijkstra_algorithm(sssp.graph)
     pprint.pprint(res)
-    assert (res == {'A': 0, 'B': 1, 'C': 1, 'D': 2, 'E': 2})
+    assert (res == {'A': (0, '-'), 'B': (1, 'A'), 'C': (1, 'A'), 'D': (2, 'C'), 'E': (2, 'B')})
+
+
+def test_bellman_ford_algorithm():
+    sssp = SingleSourceShortestPath()
+    res = sssp.bellman_ford_algorithm(sssp.graph)
+    pprint.pprint(res)
+    assert (res == {'A': (0, '-'), 'B': (1, 'A'), 'C': (1, 'A'), 'D': (2, 'C'), 'E': (2, 'B')})
+
+
+def test_bellman_ford_algorithm_neg():
+    sssp = SingleSourceShortestPath()
+    res = sssp.bellman_ford_algorithm(sssp.graph_neg)
+    pprint.pprint(res)
+    assert (res == {'A': (0, '-'), 'B': (50, 'D'), 'C': (150, 'B'), 'D': (200, 'A')})
